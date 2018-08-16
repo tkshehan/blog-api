f8a0ea0d266ad2a92561c829e00578dd2dfb8716
Thomas Shehan <thomas.shehan88@gmail.com>
2018-08-16 08:41:24 -0500

    update test dependencies

--- server.js
+++ server.js
@@ -9,6 +9,35 @@ app.use(morgan('common'));
 
 app.use('/blog-posts', blogPostsRouter);
 
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app
+      .listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve(server);
+      })
+      .on('error', err => {
+        reject(err);
+      });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing Server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
