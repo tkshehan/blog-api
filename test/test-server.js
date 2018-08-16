1312f5654ac9be2ea9babff3e8024bdace4817e2
Thomas Shehan <thomas.shehan88@gmail.com>
2018-08-16 11:03:28 -0500

    implement PUT /blog-posts testing

--- test/test-server.js
+++ test/test-server.js
@@ -5,6 +5,8 @@ const {app, runServer, closeServer} = require('../server');
 const expect = chai.expect;
 chai.use(chaiHttp);
 
+let storedId;
+let expectedLength;
 describe('Blog Posts', function() {
 
   before(function() {
@@ -32,12 +34,64 @@ describe('Blog Posts', function() {
             'publishDate'
           ]);
         });
+        // For testing PUT
+        expectedLength = res.body.length;
       });
   });
 
-  it('should add a Post on POST');
+  it('should add a Post on POST', function() {
+    const newPost = {
+      title: 'A Test To Remember',
+      author: 'Test-Server.js feat. Thomas',
+      content: 'It was the test of times, it was the worst of times...',
+      publishDate: 3
+    }
+    return chai
+      .request(app)
+      .post('/blog-posts')
+      .send(newPost)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.all.keys([
+          'id', 'title', 'author', 'content', 'publishDate'
+        ])
+        expect(res.body).to.deep.equal(
+          Object.assign(newPost, {id: res.body.id})
+        );
+        // Save for later testing
+        storedId = res.body.id;
+        expectedLength++;
+      });
+  });
 
-  it('should update a Post on PUT');
+  it('should update a Post on PUT', function() {
+    const updatedPost = {
+      title: 'A Test To Remember(We Forgot A Few Things)',
+      author: 'Test-Server.js feat. Thomas',
+      content: `It was the test of times, it was the worst of times...`,
+      publishDate: 333,
+      id: storedId,
+    }
+    return chai
+      .request(app)
+      .put(`/blog-posts/${storedId}`)
+      .send(updatedPost)
+      .then(function(res) {
+        expect(res).to.have.a.status(204);
+        expect(res.body).to.be.empty;
+      })
+      .then(function() {
+        return chai
+          .request(app)
+          .get('/blog-posts')
+      })
+      .then(function(res) {
+        expect(res.body.length).to.equal(expectedLength);
+        expect(res.body).to.deep.include(updatedPost);
+      })
+  });
 
   it('should delete a Post on DELETE');
 
