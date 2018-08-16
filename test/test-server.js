a2048fc811d892fb4ac8f439fb4fcd79e1e3a1d0
Thomas Shehan <thomas.shehan88@gmail.com>
2018-08-16 09:42:44 -0500

    fix bug in test

--- test/test-server.js
+++ test/test-server.js
@@ -22,15 +22,17 @@ describe('Blog Posts', function() {
       .then(function(res) {
         expect(res).to.be.json;
         expect(res).to.have.status(200);
-
-        const expectedKeys = [
-          'id', 'title', 'content', 'author', 'publishDate'
-        ];
         res.body.forEach(function(post) {
-          expect(item).to.be.a('object');
-          expect(post).to.include.keys(expectedKeys);
+          expect(post).to.be.a('object');
+          expect(post).to.include.all.keys([
+            'id',
+            'title',
+            'content',
+            'author',
+            'publishDate'
+          ]);
         });
-      })
+      });
   });
 
   it('should add a Post on POST');
