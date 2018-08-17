9d855afba134ac10fbe64a2ffdaa3cc65b5dbe01
Thomas Shehan <thomas.shehan88@gmail.com>
2018-08-17 18:54:38 -0500

    Implement get/put/update/delete

--- models.js
+++ models.js
@@ -7,6 +7,16 @@ const blogPostSchema = mongoose.Schema({
   publishDate: String,
 });
 
+blogPostSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content,
+    author: this.author,
+    publishDate: this.publishDate,
+  };
+};
+
 const BlogPost = mongoose.model("BlogPost", blogPostSchema);
 
 module.exports = {BlogPost};
